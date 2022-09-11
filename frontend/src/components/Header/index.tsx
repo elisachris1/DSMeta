3e3173a30ebd87f0d9b1a7e45ac93d7c6636c985
elisachris1 <elisachris1@gmail.com>
2022-09-11 13:30:24 -0700

    last adjustment a href header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
             <h1>DSMeta</h1>
             <p>
               Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig"> Elisa Rovani</a>
+              <a href="https://www.linkedin.com/in/elisa-christofaro-rovani-03637b47"> Elisa Rovani</a>
             </p>
         </div>
     </header>
